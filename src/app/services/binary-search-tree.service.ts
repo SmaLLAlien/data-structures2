c4cadbe743e9ae8c8f201b7c2d3120ba69a2e956
SmaLLAlien <vlad_belousov1990@hotmail.com>
2023-02-07 20:53:12 +0200

    binary-tree: added tree travers

--- src/app/services/binary-search-tree.service.ts
+++ src/app/services/binary-search-tree.service.ts
@@ -58,4 +58,83 @@ export class BinarySearchTreeService {
       return found;
     }
   }
+
+  breadsFirstSearch(): NodeService[] {
+    let data: NodeService[] = [];
+    let node: NodeService = this.root;
+    let queue = [node];
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return data;
+  }
+
+  depthFirstSearchPreOrder(): NodeService[] {
+    let data: NodeService[] = [];
+    let current = this.root;
+
+    this.traversePreOrder(current, data);
+    return data;
+  }
+
+  private traversePreOrder(node: NodeService, data: NodeService[]) {
+    data.push(node);
+
+    if (node.left) {
+      this.traversePreOrder(node.left, data);
+    }
+
+    if (node.right) {
+      this.traversePreOrder(node.right, data);
+    }
+  }
+
+  depthFirstSearchPostOrder(): NodeService[] {
+    let data: NodeService[] = [];
+
+    this.traversePostOrder(this.root, data);
+    return data;
+  }
+
+  private traversePostOrder(node: NodeService, data: NodeService[]) {
+    if (node.left) {
+      this.traversePostOrder(node.left, data);
+    }
+
+    if (node.right) {
+      this.traversePostOrder(node.right, data);
+    }
+
+    data.push(node);
+  }
+
+  depthFirstSearchInOrder(): NodeService[] {
+    let data: NodeService[] = [];
+
+    this.traverseInOrder(this.root, data);
+    return data;
+  }
+
+  private traverseInOrder(node: NodeService, data: NodeService[]) {
+    if (node.left) {
+      this.traverseInOrder(node.left, data);
+    }
+
+    data.push(node);
+
+    if (node.right) {
+      this.traverseInOrder(node.right, data);
+    }
+  }
 }
