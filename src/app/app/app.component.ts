5e81cff35bd7662f8e47e3741c69bc62d3cbb69e
SmaLLAlien <vlad_belousov1990@hotmail.com>
2023-02-16 14:15:32 +0200

    graphs: added graphs traversal

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
+import {GraphService} from "../services/graph.service";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent implements OnInit {
   taskControl = new FormControl('', Validators.required);
   array: (number| string)[] = [];
 
-  constructor() { }
+  constructor(private graph: GraphService) { }
 
   addItemToList(): void {
 
@@ -22,5 +23,25 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.graph.addVertex('A');
+    this.graph.addVertex('B');
+    this.graph.addVertex('C');
+    this.graph.addVertex('D');
+    this.graph.addVertex('E');
+    this.graph.addVertex('F');
+
+    this.graph.addEdge('A', 'B');
+    this.graph.addEdge('A', 'C');
+    this.graph.addEdge('B', 'D');
+    this.graph.addEdge('C', 'E');
+    this.graph.addEdge('D', 'F');
+    this.graph.addEdge('E', 'F');
+
+    const nodes = this.graph.depthFirstRecursiveTraversal('A');
+    const nodes2 = this.graph.depthFirstIterative('A');
+    const nodes3 = this.graph.breadthFirstTraversal('A');
+    console.log(nodes);
+    console.log(nodes2);
+    console.log(nodes3);
   }
 }
--- src/app/services/graph.service.ts
+++ src/app/services/graph.service.ts
@@ -37,4 +37,70 @@ export class GraphService {
     }
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursiveTraversal(startVertex: string): string[] {
+    const result: string[] = [];
+    const visited: {[key: string]: boolean} = {};
+
+    this.dfs(startVertex, visited, result);
+
+    return result;
+  }
+
+  depthFirstIterative(startNode: string) {
+    const stack: string[] = [startNode];
+    const result: string[] = [];
+    const visited: {[key: string]: boolean} = {};
+    let currVertex: string;
+
+    visited[startNode] = true;
+    while (stack.length) {
+      currVertex = stack.pop();
+      result.push(currVertex);
+
+      this.adjacencyList[currVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  breadthFirstTraversal(startNode: string): string[] {
+    const queue: string[] = [startNode];
+    const result: string[] = [];
+    const visited: {[key: string]: boolean} = {[startNode]: true};
+    let currVertex: string;
+
+    while (queue.length) {
+      currVertex = queue.shift();
+      result.push(currVertex);
+
+      this.adjacencyList[currVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  private dfs(vertex: string, visited: {[key: string]: boolean}, result: string[]) {
+    if (!vertex) {
+      return null;
+    }
+    visited[vertex] = true;
+    result.push(vertex);
+
+    this.adjacencyList[vertex].forEach(neighbor => {
+      if (!visited[neighbor]) {
+        this.dfs(neighbor, visited, result);
+      }
+    })
+  }
 }
